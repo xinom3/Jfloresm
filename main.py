d0b293b2a4a00f661b92b4eb43d4ffadaf202511
Francisco Flores <xinome@gmail.com>
2024-08-31 23:05:12 +0000

    okok

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-etry ins
+import pandas as pd
+import numpy
+import matplotlib
+import seaborn
+
+print("hola mundo")
+
+print("vamos ien  con este rollo de la programacion")
+
+c = 25
+
+
+if c==25:
+    print("vamos muy bien con este rollo") 
